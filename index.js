55cf1a75094cd6dd7311dee2a430e206956710c2
gogovintagetravel <hello@gogovintagetravel.com>
2025-05-28 16:21:39 -0500

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import QuizPage from './QuizPage';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<QuizPage />);
